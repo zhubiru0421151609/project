e41ab7c2644690e077fb408a557998af594fc8f1
zhangmin0421151622 <42957604+zhangmin0421151622@users.noreply.github.com>
2018-09-24 22:12:02 +0800

    Create VerifiedUser.h

---
+++ code/VerifiedUser.h
@@ -0,0 +1,14 @@
+#ifndef VERIFIED_USER_H
+#define VERIFIED_USER_H
+
+
+#include <string>
+#include "User.h"
+
+class VerifiedUser: public User {
+public:
+    VerifiedUser();
+    void buyMembership(int months);
+}
+
+#endif
