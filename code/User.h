fa476f0562abffb9530530761fb1b55a05749da4
zhangmin0421151622 <42957604+zhangmin0421151622@users.noreply.github.com>
2018-09-24 15:44:14 +0800

    Create User.h

---
+++ code/User.h
@@ -0,0 +1,19 @@
+#ifndef USER_H
+#define USER_H 
+
+#include <string>
+#include "UserBasicInformation.h"
+
+class User {
+public:
+    UserBasicInformation basicinfo;
+    int UID;
+    int status;
+    User();
+    User(UserBasicInformation information, int id, int status);
+    void Login();
+    void Register();
+    void Logout();
+};
+
+#endif
