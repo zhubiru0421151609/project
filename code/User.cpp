b6587e27aae81bdc3346eac8419d354cf2223dac
zhangmin0421151622 <42957604+zhangmin0421151622@users.noreply.github.com>
2018-09-24 20:56:25 +0800

    Update User.cpp

--- code/User.cpp
+++ code/User.cpp
@@ -1,5 +1,36 @@
 #ifndef USER_CPP
 #define USER_CPP
 
+#include <string>
+#include "User.h"
+
+User::User() {
+    this->UID = -1;
+    this->status = -1;
+};
+
+User::User(UserBasicInformation information, int id, int status) {
+    this->basicinfo = information;
+    this->UID = id;
+    this->status = status;
+};
+
+void User::Login() {
+    if (UID != -1) 
+        this->status = 1;
+    else this->status = -1;
+};
+
+void User::Register(UserBasicInformation information, int id) {
+    this->basicinfo = information;
+    this->UID = id;
+    status = 0;
+};
+
+void User::Logout() {
+    if (UID != -1)
+        this->status = 0;
+    else this->status = -1;
+};
 
 #endif
