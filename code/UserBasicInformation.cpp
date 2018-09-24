d53eff821e7b32154e60781c05d4caff125e7152
zhangmin0421151622 <42957604+zhangmin0421151622@users.noreply.github.com>
2018-09-24 15:19:57 +0800

    Create UserBasicInformation.cpp

---
+++ code/UserBasicInformation.cpp
@@ -0,0 +1,59 @@
+#ifndef USER_BASIC_INFORMATION_CPP
+#define USER_BASIC_INFORMATION_CPP
+
+#include <string>
+#include "UserBasicInformation.h"
+
+UserBasicInformation UserBasicInformation::get() {
+    return *this;
+};
+
+UserBasicInformation::UserBasicInformation() {
+    this->username ="";
+    this->password = "";
+    this->mailbox = "";
+    this->telephone = "";
+    this->gender = "";
+    this->nation = "";
+    this->age = "";
+    this->native_language = "";
+    this->interested_language = "";
+    this->id_card_number = "";
+};
+
+UserBasicInformatino::UserBasicInformation(string name, string pwd, string mail, string tele, string gen, string nation, string age) {
+    this->username = name;
+    this->password = pwd;
+    this->mailbox = mail;
+    this->telephone = tele;
+    this->gender = gen;
+    this->nation = nation;
+    this->age = age;
+    this->native_language = "";
+    this->interested_language = "";
+    this->id_card_number = "";
+};
+
+void UserBasicInformation::set(string name, string pwd, string mail, string tele, string gen, string nation, string age) {
+    this->username = name;
+    this->password = pwd;
+    this->mailbox = mail;
+    this->telephone = tele;
+    this->gender = gen;
+    this->nation = nation;
+    this->age = age;
+    this->native_language = ""
+    this->interested_language = "";
+    this->id_card_number = "";
+};
+
+void UserBasicInformation::setIdCardNumber(int id) {
+    this->id_card_number = id;
+}
+
+void UserBasicInformation::setLanguage(string native, string interested) {
+    this->native_language = native;
+    this->interested_language = interested;
+}
+
+#endif
