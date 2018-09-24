37ac72a189c5a944d6675c02b2abcab9715046a5
zhangmin0421151622 <42957604+zhangmin0421151622@users.noreply.github.com>
2018-09-24 21:23:16 +0800

    Create ChattingRoomInfo.cpp

---
+++ code/ChattingRoomInfo.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include "ChattingRoomInfo.h"
+
+ChattingRoomInfo::ChattingRoomInfo() {
+    this->topic = "";
+};
+
+ChattingRoomInfo ChattingRoomInfo::get() {
+    return this;
+};
+
+void ChattingRoomInfo::set(string topic, VIP u1, VIP u2) {
+    this->topic = topic;
+    this->user1 = u1;
+    this->user2 = u2;
+}
