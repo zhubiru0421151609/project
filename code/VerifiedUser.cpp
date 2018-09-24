56e348e44f62cd5f084858bf50eb84d32db3fe2f
zhangmin0421151622 <42957604+zhangmin0421151622@users.noreply.github.com>
2018-09-24 22:16:40 +0800

    Create VerifiedUser.cpp

---
+++ code/VerifiedUser.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "VerifiedUser.h"
+
+VerifiedUser::VerifiedUser() {
+    this->lastMembership = 0;
+}
+
+void VerifiedUser::bugMembership(int months) {
+    bool success = moneyShouldPay(months);
+    if (success) 
+        this->lastMembership += months;
+}
